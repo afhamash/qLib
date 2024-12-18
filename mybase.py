ffebbcae23ce1dc15f790b57d6b4b669f2f7009a
afhamash <afhamash@gmail.com>
2024-12-18 19:56:43 +0530

    Minor commit after Initial commit.

---
+++ mybase.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+
+from numpy import linalg as la
+from scipy import linalg as sla
+from numpy.linalg import trace as tr
+from numpy import sqrt as sr
+from scipy.linalg import inv as inv
+from scipy.linalg import pinv as pinv
+from numpy.linalg import norm
